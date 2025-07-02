2b2de894ad6fb7fefdeb7fb9f7674611c559a6a8
Olawale <olawalesodiq988@gmail.com>
2025-07-02 05:48:12 +0100

    Refactor cart and index pages: remove title from cart, enhance index layout, and improve mobile menu functionality

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// This is for the search icon
+
 const searchIcon = document.querySelector(".search-icon");
 const searchBar = document.querySelector(".search-container");
 // const accountCreation = document.querySelector(".Account");
@@ -7,6 +9,33 @@ const searchBar = document.querySelector(".search-container");
 searchIcon.addEventListener("click", function() {
     searchBar.style.display = "flex";
     searchIcon.style.display = "none";
+});
+
+let closeIcon = document.querySelector(".close-menu");
+closeIcon.addEventListener("click", function () {
+    searchBar.style.display = "none";
+    searchIcon.style.display = "flex";
+});
+
+
+// This is for the menu icon
+
+let menuBtn = document.querySelector(".menu");
+let hiddenMenu = document.querySelector(".mobile-menu");
+let hiddenClose = document.querySelector(".hidden-close");
+
+
+
+menuBtn.addEventListener("click", function () {
+    menuBtn.style.display = "none";
+    hiddenClose.style.display = "block";
+    hiddenMenu.style.display = "block";
+});
+
+hiddenClose.addEventListener("click", function () {
+    menuBtn.style.display = "flex";
+    hiddenMenu.style.display = "none";
+    hiddenClose.style.display = "none";
 });
 
 
