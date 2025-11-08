70183f270296c6038b96bafaf186c5c4d7e07316
Olawale <olawalesodiq988@gmail.com>
2025-11-08 17:51:12 +0100

    Woking on the slider functionality

--- main.js
+++ main.js
@@ -1,51 +1,88 @@
 "use strict";
 
-// This is for the search icon
+    // This is for the search icon
+    const searchIcon = document.querySelector(".search-icon");
+    const searchBar = document.querySelector(".search-container");
 
-const searchIcon = document.querySelector(".search-icon");
-const searchBar = document.querySelector(".search-container");
-// const accountCreation = document.querySelector(".Account");
+    if (searchIcon) {
+        searchIcon.addEventListener("click", function() {
+            searchBar.style.display = "flex";
+            searchIcon.style.display = "none";
+            menuBtn.style.display = "flex";
+            hiddenMenu.style.display = "none";
+            hiddenClose.style.display = "none";
+        });
+    }
 
-searchIcon.addEventListener("click", function() {
-    searchBar.style.display = "flex";
-    searchIcon.style.display = "none";
-    menuBtn.style.display = "flex";
-    hiddenMenu.style.display = "none";
-    hiddenClose.style.display = "none";
-});
+    const closeIcon = document.querySelector(".close-menu");
+    if (closeIcon) {
+        closeIcon.addEventListener("click", function () {
+            searchBar.style.display = "none";
+            searchIcon.style.display = "flex";
+            menuBtn.style.display = "flex";
+            hiddenMenu.style.display = "none";
+            hiddenClose.style.display = "none";
+        });
+    }
 
-let closeIcon = document.querySelector(".close-menu");
-closeIcon.addEventListener("click", function () {
-    searchBar.style.display = "none";
-    searchIcon.style.display = "flex";
-    menuBtn.style.display = "flex";
-    hiddenMenu.style.display = "none";
-    hiddenClose.style.display = "none";
-});
+    // This is for the menu icon
+    const menuBtn = document.querySelector(".menu");
+    const hiddenMenu = document.querySelector(".mobile-menu");
+    const hiddenClose = document.querySelector(".hidden-close");
+
+    if (menuBtn) {
+        menuBtn.addEventListener("click", function () {
+            menuBtn.style.display = "none";
+            hiddenClose.style.display = "block";
+            hiddenMenu.style.display = "block";
+            searchBar.style.display = "none";
+            searchIcon.style.display = "flex";
+        });
+    }
+
+    if (hiddenClose) {
+        hiddenClose.addEventListener("click", function () {
+            menuBtn.style.display = "flex";
+            hiddenMenu.style.display = "none";
+            hiddenClose.style.display = "none";
+            searchBar.style.display = "none";
+            searchIcon.style.display = "flex";
+        });
+}
 
 
-// This is for the menu icon
+    
+// Slider Functionality
 
-let menuBtn = document.querySelector(".menu");
-let hiddenMenu = document.querySelector(".mobile-menu");
-let hiddenClose = document.querySelector(".hidden-close");
+const sliderWrapper = document.querySelector('.slider-wrapper');
+
+const sliderWrapper2 = document.querySelector('.slider-wrapper2');
+
+function getScrollPercentage() {
+    const leftToRight = sliderWrapper.scrollLeft;
+    const maxScroll = sliderWrapper.scrollWidth - sliderWrapper.clientWidth;
+    console.log({leftToRight, maxScroll});
+    return maxScroll > 0 ? (leftToRight / maxScroll) * 100 : 0;
+}
+
+function prevBtn() {
+    sliderWrapper.scrollLeft -= 200;
+    getScrollPercentage();
+}
+
+function nextBtn() {
+    sliderWrapper.scrollLeft += 200;
+    getScrollPercentage();
+}
 
 
+function prevBtn2() {
+    sliderWrapper2.scrollLeft -= 200;
+    getScrollPercentage();
+}
 
-menuBtn.addEventListener("click", function () {
-    menuBtn.style.display = "none";
-    hiddenClose.style.display = "block";
-    hiddenMenu.style.display = "block";
-    searchBar.style.display = "none";
-    searchIcon.style.display = "flex";
-});
-
-hiddenClose.addEventListener("click", function () {
-    menuBtn.style.display = "flex";
-    hiddenMenu.style.display = "none";
-    hiddenClose.style.display = "none";
-    searchBar.style.display = "none";
-    searchIcon.style.display = "flex";
-});
-
+function nextBtn2() {
+    sliderWrapper2.scrollLeft += 200;
+    getScrollPercentage();
+}
 
